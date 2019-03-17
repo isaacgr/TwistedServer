baabe2d96662d2d652d1df510601d4f59caa0691
Isaac Rowell <isaacgrowell@gmail.com>
2019-03-17 10:20:38 -0400

    Class to get the internet ip address of the server

---
+++ client/internet_addr.py
@@ -0,0 +1,17 @@
+from twisted.internet import defer, reactor
+from subprocess import Popen, PIPE
+
+class InterAddr(object):
+
+    def __init__(self):
+        pass
+
+    def get_addr(self):
+        print 'Getting internet ip'
+        p = Popen(["curl", "https://ipinfo.io/ip"], stdin=PIPE, stdout=PIPE, stderr=PIPE)
+        output, error = p.communicate()
+        output = output.rstrip("\n")
+        if not p.returncode:
+            return {'ip': output}
+        return [error, p.returncode]
+
