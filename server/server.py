6b828acfc9ef585ace8de29485ec03f59a528477
Isaac Rowell <isaacgrowell@gmail.com>
2020-02-25 17:42:49 -0500

    Update server.py with actual implementation

--- server/server.py
+++ server/server.py
@@ -1,14 +1,17 @@
 from twisted.web.static import File
-from klein import Klein
+from twisted.internet.defer import succeed
+from klein import run, route
+from requests.auth import HTTPDigestAuth
+import requests
 
-app = Klein()
+base_url = "http://192.168.2.46/cgi-bin/ptz.cgi?action=start&channel=0&code=GotoPreset&arg1=0&arg2=%s&arg3=0&arg4=0"
 
-@app.route('/', branch=True)
-def home(request):
-    return File('./public')
+# stupid digest auth requires requests, which is blocking
+@route('/api/preset', methods=['POST'])
+def set_camera(request):
+    preset = request.args.get('preset', [1])[0]
+    url = base_url % preset
+    requests.post(url, auth=HTTPDigestAuth('username', 'password'))
+    return succeed('OK')
 
-@app.route('/about')
-def pg_about(request):
-    return 'The about page'
-
-app.run('192.168.2.48', 8080)
+run('192.168.2.48', 8080)
